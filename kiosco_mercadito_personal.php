d107dc964e842dff68736744395c35f3723407a6
clonehugger <andrea.puente@udem.edu>
2018-07-23 14:45:27 -0500

    arreglo mercado en perfil

--- kiosco_mercadito_personal.php
+++ kiosco_mercadito_personal.php
@@ -9,71 +9,76 @@
       while($d=mysqli_fetch_assoc($q1)){
       ?>
 
-    <div class="col s4">
-      <div class="card amber accent-1">
-        <div class="card-content black-text">
-          <span class="card-title black-text center"><?php echo $d["nombre"]; ?></span>
-          <div class="divider"></div>
-          <div class="section">
-            <p><?php echo $d["first_name"]." ".$d["last_name"]; ?></p>
-            <p><?php echo $d["telefono"]?></p>
-            <p><?php echo "$".$d["precio"]; ?></p>
+      <div class="col s4">
+        <div class="card amber accent-1" style="border-radius: 20px;">
+          <div class="card-content black-text">
+            <span class="card-title black-text center amber accent-2" style="border-radius: 20px 20px 0px 0px; padding-top:15px; padding-bottom: 10px; margin: -25px -25px 10px -25px; font-weight: bold;"><?php echo $d["nombre"]; ?></span>
+            <div class="section">
+              <p class = "dueño grey-text text-darken-1">
+                <div class="valign-wrapper left" style="width:21px; margin-right: 7px;">
+                  <img src="img/<?php echo $d["foto"];?>.png" alt="" class="circle responsive-img">
+                </div>
+                <?php echo $d["first_name"]." ".$d["last_name"]; ?>
+              </p>
+              <p><i class="material-icons prefix">phone</i> <span style="position: relative; top: -5px;"><?php echo $d["telefono"]?></span></p>
+              <p><i class="material-icons prefix">attach_money</i> <span style="position: relative; top: -5px;"><?php echo $d["precio"]; ?></span></p>
+            </div>
+            <div class = "center">
+              <a href="#modalProduct<?php echo $d['ventaID'];?>" class="white-text modal-trigger btn" style="background-color: #5F77B7">Detalles</a>
+            </div>
           </div>
-          </div>
-          <div class="card-action white-text">
-            <a href="#modalProduct<?php echo $d['ventaID'];?>" class="white-text modal-trigger btn">Mas informacion....</a>
-          </div>
-        </div>
 
-        <div id="modalProduct<?php echo $d['ventaID'];?>" class="modal modal-fixed-footer">
-            <div class="modal-content">
-              <div class="left-align">
-                <h4 class="center"><?php echo $d['nombre'] ?></h4>
-                <div class="divider"></div>
-                <div class="row">
-                  <div class="col s6">
-                    <div class="section">
-                      <i class="material-icons prefix">account_circle</i>
-                      <?php echo $d["first_name"]." ".$d["last_name"]; ?>
+          </div>
+
+          <div id="modalProduct<?php echo $d['ventaID'];?>" class="modal modal-fixed-footer">
+              <div class="modal-content">
+                <div class="left-align">
+                  <h4 class="center"><?php echo $d['nombre'] ?></h4>
+                  <div class="divider"></div>
+                  <div class="row">
+                    <div class="col s6">
+                      <div class="section">
+                        <i class="material-icons prefix">account_circle</i>
+                        <?php echo $d["first_name"]." ".$d["last_name"]; ?>
+                      </div>
+                      <div class="section">
+                        <i class="material-icons prefix">phone</i>
+                        <?php echo $d["telefono"]?>
+                      </div>
+                      <div class="section">
+                        <i class="material-icons prefix">attach_money</i>
+                        <?php echo $d["precio"]?>
+                      </div>
+                      <div class="section">
+                        <i class="material-icons prefix">calendar_today</i>
+                         del <?php echo date('d/m/Y',strtotime($d["fecha_ini"]));?> hasta <?php echo date('d/m/Y',strtotime($d["fecha_fin"]));?>
+                      </div>
+                      <div class="section">
+                        <i class="material-icons prefix">access_time</i>
+                        desde las <?php echo $d["hora"]?>
+                      </div>
                     </div>
-                    <div class="section">
-                      <i class="material-icons prefix">phone</i>
-                      <?php echo $d["telefono"]?>
-                    </div>
-                    <div class="section">
-                      <i class="material-icons prefix">attach_money</i>
-                      <?php echo $d["precio"]?>
-                    </div>
-                    <div class="section">
-                      <i class="material-icons prefix">calendar_today</i>
-                       del <?php echo date('d/m/Y',strtotime($d["fecha_ini"]));?> hasta <?php echo date('d/m/Y',strtotime($d["fecha_fin"]));?>
-                    </div>
-                    <div class="section">
-                      <i class="material-icons prefix">access_time</i>
-                      desde las <?php echo $d["hora"]?>
-                    </div>
-                  </div>
-                  <div class="col s6">
-                    <div class="section">
-                      <i class="material-icons prefix">description</i> Descripcion:
-                      <div class="divider"></div>
-                      <p><?php echo $d['descripcion'] ?></p>
+                    <div class="col s6">
+                      <div class="section">
+                        <i class="material-icons prefix">description</i> Descripcion:
+                        <div class="divider"></div>
+                        <p><?php echo $d['descripcion'] ?></p>
+                      </div>
                     </div>
                   </div>
                 </div>
+
               </div>
 
-            </div>
-
-            <div class="modal-footer">
-              <div class="center">
-                <a class="modal-close btn waves-effect waves-light red"><i class="material-icons">close</i></a>
+              <div class="modal-footer">
+                <div class="center">
+                  <a class="modal-close btn waves-effect waves-light red"><i class="material-icons">close</i></a>
+                </div>
               </div>
+            </form>
             </div>
-          </form>
-          </div>
 
-    </div>
+      </div>
 
 
 
