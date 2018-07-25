aac6d816729475775710f3ab48f25fb687b27376
clonehugger <andrea.puente@udem.edu>
2018-07-25 17:06:36 -0500

    Tabla de tresultados de votacion

--- kiosco_pregunta_resultado.php
+++ kiosco_pregunta_resultado.php
@@ -9,28 +9,73 @@
   </div>
 
   <div class = "col s11 blue lighten-4 low-text left-align " style="border:none; border-radius: 0px 20px 20px 0px;align-items: stretch;">
-      <div class ="col s9">
+      <div class ="col s7">
       <div class = "titulo flow-text"><?php echo $row[9];?></div>
-
-      <?php // aux query for user
-        $aux_query = "SELECT first_name, last_name FROM Users WHERE id = $row[3]";
-        $aux_result = mysqli_query($conexion, $aux_query) or die ("Error de consulta ".mysqli_error());
-        $aux_row = mysqli_fetch_row($aux_result);
-      ?>
-
       <div class = "grey-text text-darken-1"><?php echo "Pregunta de encuesta: ";?></div>
       <?php echo $row[2];?>
       <br><br>
 
     </div>
-    <div class = "col s3">
+    <div class = "col s5">
       <div class = "row center">
         <br>
-        Resultados:
+        <table class = "highlight">
+        <thead>
+          <tr>
+              <th>Respuesta</th>
+              <th>Votos </th>
+          </tr>
+        </thead>
 
-        <!-- Modal Trigger -->
+        <tbody>
+          <?php
+            if ($row[6] != NULL ){?>
+              <?php // aux query for user
+                $aux_query = "SELECT count(*) FROM Voto WHERE publicacionID = $row[0] AND voto = 1;";
+                $aux_result = mysqli_query($conexion, $aux_query) or die ("Error de consulta ".mysqli_error());
+                $aux_row = mysqli_fetch_row($aux_result);
+              ?>
+            <tr>
+              <td><?php echo $row[6];?></td>
+              <td><?php echo $aux_row[0];?></td>
+            </tr>
+          <?php } ?>
+          <?php
+          if ($row[7] != NULL ){?>
+            <?php // aux query for user
+              $aux_query = "SELECT count(*) FROM Voto WHERE publicacionID = $row[0] AND voto = 2;";
+              $aux_result = mysqli_query($conexion, $aux_query) or die ("Error de consulta ".mysqli_error());
+              $aux_row = mysqli_fetch_row($aux_result);
+            ?>
+          <tr>
+            <td><?php echo $row[7];?></td>
+            <td><?php echo $aux_row[0];?></td>
+          </tr>
+          <?php } ?>
+          <?php
+          if ($row[8] != NULL ){?>
+            <?php // aux query for user
+              $aux_query = "SELECT count(*) FROM Voto WHERE publicacionID = $row[0] AND voto = 3;";
+              $aux_result = mysqli_query($conexion, $aux_query) or die ("Error de consulta ".mysqli_error());
+              $aux_row = mysqli_fetch_row($aux_result);
+            ?>
+          <tr>
+            <td><?php echo $row[8];?></td>
+            <td><?php echo $aux_row[0];?></td>
+          </tr>
+          <?php } ?>
+          <?php // aux query for user
+            $aux_query = "SELECT count(*) FROM Voto WHERE publicacionID = $row[0] AND voto = 4;";
+            $aux_result = mysqli_query($conexion, $aux_query) or die ("Error de consulta ".mysqli_error());
+            $aux_row = mysqli_fetch_row($aux_result);
+          ?>
+          <tr>
+            <td>Ninguno</td>
+            <td><?php echo $aux_row[0];?></td>
+          </tr>
+        </tbody>
+      </table>
 
-        <button data-target="modal-question<?php echo $row[0];?>" id = "boton-votar<?php echo $row[0];?>" class="modal-trigger btn-large" style="background-color: #5F77B7"> <div class = "col s12">Votar</div></button>
 
 
       </div>
