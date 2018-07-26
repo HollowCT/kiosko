297af85c9dad8b1b37aa596d6c6982bfd8d2d24b
HollowCT <ctinopearl@gmail.com>
2018-07-26 15:21:01 -0500

    update to disable right click

--- disable_right_click.php
+++ disable_right_click.php
@@ -1,14 +1,22 @@
 <script language="JavaScript">
+
+$(document).on("keydown",function(ev){
+	console.log(ev.keyCode);
+	if(ev.keyCode===27||ev.keyCode===122) return false
+})
+
   window.onload = function() {
     document.addEventListener("contextmenu", function(e){
       e.preventDefault();
     }, false);
+
     document.addEventListener("keydown", function(e) {
       // "F11" key
       if (event.keyCode == 122) {
         disabledEvent(e);
       }
     }, false);
+
     function disabledEvent(e){
       if (e.stopPropagation){
         e.stopPropagation();
